cd07ea4a49dbedf31371ba0d6e9b2d7ca495a80d
FUNDAMENTOS DE PROGRAMACIÓN <je.landazuri@uea.edu.ec>
2024-08-28 23:27:40 -0500

    Tarea de la Semana 11 Búsqueda y Ordenación en Arreglos Multidimensionales

--- Undiad 3.2. Operaciones con arreglos multidimensionales/Ejercicio 2. Ordenación de Arreglo Multidimensional.py
+++ Undiad 3.2. Operaciones con arreglos multidimensionales/Ejercicio 2. Ordenación de Arreglo Multidimensional.py
@@ -1,9 +1,27 @@
 #Elaborado por: Jhon Landazuri
 #Ordenar Arreglo Multidimensional
 
-print("Tarea Semaa 11: Ejercicio 1")
+print("Tarea Semaa 11: Ejercicio 2")
 matriz = [
-    [11, 2, 23],
-    [22, 1, 00],
-    [27, 1, 1]
+    [7, 4, 99],
+    [9, 69, 10],
+    [11, 42, 5]
 ]
+print(matriz)
+
+#Se realizara el metodo de ordenamiento buble_sort
+
+def buble_sort(fila):
+    #Elaboracion del algoritmo buble Sort
+    n = len(fila)
+    for i in range(n):
+        for j in range(n-1, i, -1):
+            if fila[j] > fila[i]:
+                fila[j], fila[j-1] = fila[j-1], fila[j]
+                print(fila)
+print("Matriz Original")
+print(matriz)
+buble_sort(matriz[2])
+print(matriz)
+
+
